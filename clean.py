b689f766e94107aadb68149b5d3014ed607939e9
Rhythm77-step <tripathirhythm247@gmail.com>
2026-07-15 14:37:19 +0000

    update code

--- clean.py
+++ clean.py
@@ -3,9 +3,10 @@ import pandas as pd
 import numpy as np 
 
 #loading the dataset 
-df  = pd.read_csv("")
+df  = pd.read_csv("dirty_employee_dataset_1200.csv")
 print(df.head())
 #checking the missing values
 print("no of missing values")
 print(df.isnull().sum())
+print("add")
 
